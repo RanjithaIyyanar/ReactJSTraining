2bec674ce0737ff7d09b556c86f0a95137feada4
RanjithaIyyanar <87688314+RanjithaIyyanar@users.noreply.github.com>
2024-01-19 12:21:42 +0530

    Add files via upload
    Initial Commit

---
+++ Day2/Main.js
@@ -0,0 +1,10 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "heading" }, "I'm Heading 1"),
+    React.createElement("h2", { id: "heading2" }, "I'm Heading 2"),
+  ])
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
