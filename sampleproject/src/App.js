09217ff5ef883c589ab2058e1903a008ceb20f99
RanjithaIyyanar <ranpri35.i@gmail.com>
2024-01-29 14:56:02 +0530

    Initial commit for The Techie Blog

---
+++ sampleproject/src/App.js
@@ -0,0 +1,12 @@
+import NavBar from "./navbar/navbar";
+import Footer from "./footer/footer";
+
+function App() {
+  return (
+    <div>
+        <NavBar/><Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ sampleproject/src/footer/footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./footer.css";
+
+export default function Footer() {
+  return (
+    // <div>footer</div>
+    <footer class="footer-Con">
+        <i class="fa fa-facebook-official w3-hover-opacity"></i>
+        <i class="fa fa-instagram w3-hover-opacity"></i>
+        <i class="fa fa-snapchat w3-hover-opacity"></i>
+        <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+        <i class="fa fa-twitter w3-hover-opacity"></i>
+        <i class="fa fa-linkedin w3-hover-opacity"></i>
+        <p class="w3-medium">Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+      </footer>
+  )
+}
